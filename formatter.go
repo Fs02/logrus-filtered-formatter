142f8b772ad7e2e4cba3b21f824ee353c8b7c0d6
Fs02 <surya.asriadie@gmail.com>
2017-09-27 17:42:18 +0700

    wip formatter

---
+++ formatter.go
@@ -0,0 +1,29 @@
+package filtered
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Formatter formats logs with filtered fields
+type Formatter struct {
+	formatter logrus.Formatter
+	filter    *regexp.Regexp
+}
+
+// New returns an instance of logrus formatter
+func New(fields []string, formatter logrus.Formatter) *Formatter {
+	return &Formatter{
+		formatter: formatter,
+		filter:    regexp.MustCompile(`(` + strings.Join(fields, "|") + `):(\s*")*(\d*|true|false|"([^"]*)")("*)(,|})`),
+	}
+}
+
+// Format renders a single log entry
+func (f *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
+	data, err := f.formatter.Format(entry)
+	data = f.filter.ReplaceAll(data, []byte(`$1:$2[FILTERED]$5$6`))
+	return data, err
+}
