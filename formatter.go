87afc5bf5db1fc8b78372f6e17c88e2c28c7b13e
Muhammad Surya <surya.asriadie@gmail.com>
2017-09-27 18:07:12 +0700

    Update formatter.go

--- formatter.go
+++ formatter.go
@@ -17,13 +17,13 @@ type Formatter struct {
 func New(fields []string, formatter logrus.Formatter) *Formatter {
 	return &Formatter{
 		formatter: formatter,
-		filter:    regexp.MustCompile(`(` + strings.Join(fields, "|") + `):(\s*")*(\d*|true|false|"([^"]*)")("*)(,|})`),
+		filter:    regexp.MustCompile(`"(` + strings.Join(fields, "|") + `)":\s*"*(\d*|true|false|([^"]*)")"*(,\s*|\s*\n?\s*})`),
 	}
 }
 
 // Format renders a single log entry
 func (f *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
 	data, err := f.formatter.Format(entry)
-	data = f.filter.ReplaceAll(data, []byte(`$1:$2[FILTERED]$5$6`))
+	data = f.filter.ReplaceAll(data, []byte(`"$1":[FILTERED]$4`))
 	return data, err
 }
